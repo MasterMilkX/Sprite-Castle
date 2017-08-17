28b7d43610acc35868908d11d44d227948fbfc03
Milk Charity <mastermilkx@gmail.com>
2017-08-17 15:57:56 -0400

    Version 0.04 - Functional game

--- Game.java
+++ Game.java
@@ -16,6 +16,7 @@ public class Game{
 	public static boolean addTrees = true;
 	public static boolean addWater = false;
 	public static boolean game_over = false;
+	public static String game_mode = "HARD";
 
 	//values
 	public static int sprite_size = 8;
@@ -33,9 +34,9 @@ public class Game{
 	static Image bipi;
 	static Image bridge_h;
 	static Image bridge_v;
-	static Image castle;
-	static Image hammer;
-	static Image key;
+	static Image castle_img;
+	static Image hammer_img;
+	static Image key_img;
 	static Image tree;
 	static Image yeet;
 
@@ -43,8 +44,15 @@ public class Game{
 	public static Player p1 = new Player();
 
 	//npc info
-	public int max_enemy = 7;
+	public static int max_enemy = 7;
 	public static List<Enemy> enemies = new ArrayList<Enemy>();
+
+	//item info
+	public static int max_items = 4;
+	public static List<Item> items = new ArrayList<Item>();
+
+	//castle info
+	public static Castle castle = new Castle();
 
 	//frames and panels
 	public static JFrame window = new JFrame("Sprite Castle - by Milk");
@@ -56,6 +64,7 @@ public class Game{
 	public static class Player{
 		public int x = map_size/2;
 		public int y = map_size/2;
+		public boolean show = true;
 
 		public int hammer = 0;
 		public int key = 0;
@@ -75,9 +84,23 @@ public class Game{
 		}
 
 		public void walk(String dir){
+			if(!show)
+				return;
+
 			//System.out.println("Hey I'm walking here!");
 			if(collide(dir)){
 				System.out.println("ow");
+				return;
+			}
+			String nearCastle = hasKey(dir);
+			if(nearCastle.equals("enter")){
+				System.out.println("rescued!");
+				castles++;
+				key--;
+				grid.resetMap();
+				return;
+			}else if(nearCastle.equals("reject")){
+				System.out.println("you shall not pass");
 				return;
 			}
 
@@ -105,16 +128,82 @@ public class Game{
 		}
 		public void randomPlace(){
 			int rx;
-  			int ry;
-  			do{
-  				rx = (int)(Math.floor(Math.random() * map_size));
-  				ry = (int)(Math.floor(Math.random() * map_size));
-  			}while(terrain_map[ry][rx] != 0);
+			int ry;
+			do{
+				rx = (int)(Math.floor(Math.random() * map_size));
+				ry = (int)(Math.floor(Math.random() * map_size));
+			}while(terrain_map[ry][rx] != 0);
 
-  			this.x = rx;
-  			this.y = ry;
+			this.x = rx;
+			this.y = ry;
 		}
 
+		public void pickup(Item item){
+			if(item.show){
+				if(item.name.equals("hammer")){
+					this.hammer++;
+					System.out.println("hammer time");
+				}else if(item.name.equals("key")){
+					this.key++;
+					System.out.println("key of YEET!");
+				}else{
+					System.out.println("wut");
+				}
+				item.show = false;
+			}
+			
+		}
+
+		public String hasKey(String dir){
+			int nextX = p1.x;
+			int nextY = p1.y;
+
+			if(dir.equals("north")){
+				nextY--;
+			}else if(dir.equals("south")){
+				nextY++;
+			}else if(dir.equals("west")){
+				nextX--;
+			}else if(dir.equals("east")){
+				nextX++;
+			}
+
+			if(castle.x == nextX && castle.y == nextY && key>0){
+				return "enter";
+			}else if(castle.x == nextX && castle.y == nextY && key==0){
+				return "reject";
+			}else{
+				return "no castle";
+			}
+		}
+
+	}
+
+	public static class Item{
+		public String name;
+		public int x;
+		public int y;
+		public boolean show = true;
+
+		public Item(String name){
+			this.name = name;
+
+			//random place
+			int rx;
+			int ry;
+			do{
+				rx = (int)(Math.floor(Math.random() * map_size));
+				ry = (int)(Math.floor(Math.random() * map_size));
+			}while(terrain_map[ry][rx] != 0);
+
+			this.x = rx;
+			this.y = ry;
+		}
+		public Item(String name, int x, int y){
+			this.name = name;
+			this.x = x;
+			this.y = y;
+		}
 	}
 
 	public static class Enemy{
@@ -122,45 +211,106 @@ public class Game{
 		public int y;
 		public boolean show = true;
 
+		public Enemy(){
+			//random place
+			int rx;
+			int ry;
+			do{
+				rx = (int)(Math.floor(Math.random() * map_size));
+				ry = (int)(Math.floor(Math.random() * map_size));
+			}while(terrain_map[ry][rx] != 0);
+
+			this.x = rx;
+			this.y = ry;
+		}
 		public Enemy(int x, int y){
 			this.x = x;
 			this.y = y;
 			show = true;
 		}
+		
 		//movement
 		public void walk(){
-			List<String> directions = Arrays.asList("north", "west", "east", "south");
-			//eliminate some options
-			if(x == 0){
-				directions.remove("west");
-			}if(y == 0){
-				directions.remove("north");
-			}if(x == map_size-1){
-				directions.remove("east");
-			}if(y == map_size-1){
-				directions.remove("south");
-			}
+			if(show){
+				ArrayList<String> directions = new ArrayList<String>();
+				directions.add("north");
+				directions.add("south");
+				directions.add("east");
+				directions.add("west");
 
-			//walk
-			int r = (int)(Math.floor(Math.random()*directions.size()));
-			switch(directions.get(r)){
-				case "north": y--;break;
-				case "south": y++;break;
-				case "west": x--;break;
-				case "east":x++;break;
-				default: break;
+				//List<String> directions = Arrays.asList("north", "west", "east", "south");
+				//eliminate some options
+				if(x == 0){
+					directions.remove("west");
+				}if(y == 0){
+					directions.remove("north");
+				}if(x == map_size-1){
+					directions.remove("east");
+				}if(y == map_size-1){
+					directions.remove("south");
+				}
+
+				if(directions.isEmpty()){
+					return;
+				}
+
+				for(int d=0;d<directions.size();d++){
+					if(collide(directions.get(d))){
+						directions.remove(d);
+					}
+				}
+
+				if(directions.isEmpty()){
+					return;
+				}
+
+				//walk
+				int r = (int)(Math.floor(Math.random()*directions.size()));
+				switch(directions.get(r)){
+					case "north": y--;break;
+					case "south": y++;break;
+					case "west": x--;break;
+					case "east":x++;break;
+					default: break;
+				}
 			}
 			return;
 		}
-		public void duel(Player p){
-			if((p.x == x) && (p.y == y) && show){
+		
+		/*
+
+		public void walk(){
+			ArrayList<String> directions = new ArrayList<String>();
+				directions.add("north");
+				directions.add("south");
+				directions.add("east");
+				directions.add("west");
+
+				int r = (int)(Math.floor(Math.random()*directions.size()));
+				
+				System.out.println(directions.get(r));
+				switch(directions.get(r)){
+					case "north": y--;break;
+					case "south": y++;break;
+					case "west": x--;break;
+					case "east":x++;break;
+					default: break;
+				}
+		}
+
+		*/
+
+		public void duel(){
+			if((p1.x == x) && (p1.y == y) && show){
 				//enemy dead
-				if(p.hammer > 0){
-					p.hammer--;
+				if(p1.hammer > 0){
+					p1.hammer--;
+					p1.enemies++;
 					show = false;
 				}
 				//player dead
 				else{
+					p1.show = false;
 					game_over = true;
 				}
 			}
@@ -179,6 +329,53 @@ public class Game{
 				return false;
 			}
 		}
+
+		public void randomPlace(){
+			int rx;
+			int ry;
+			do{
+				rx = (int)(Math.floor(Math.random() * map_size));
+				ry = (int)(Math.floor(Math.random() * map_size));
+			}while(terrain_map[ry][rx] != 0);
+
+			this.x = rx;
+			this.y = ry;
+		}
+	}
+
+	public static class Castle{
+		public int x;
+		public int y;
+
+		public Castle(){
+			//random place
+			int rx;
+			int ry;
+			do{
+				rx = (int)(Math.floor(Math.random() * map_size));
+				ry = (int)(Math.floor(Math.random() * map_size));
+			}while(terrain_map[ry][rx] != 0);
+
+			this.x = rx;
+			this.y = ry;
+		}
+		public Castle(int x, int y){
+			this.x = x;
+			this.y = y;
+		}
+
+		public void randomPlace(){
+			int rx;
+			int ry;
+			do{
+				rx = (int)(Math.floor(Math.random() * map_size));
+				ry = (int)(Math.floor(Math.random() * map_size));
+			}while(terrain_map[ry][rx] != 0);
+
+			this.x = rx;
+			this.y = ry;
+		}
+
 	}
 
 	public static class GridMap extends JPanel implements KeyListener{
@@ -190,27 +387,101 @@ public class Game{
 			getIMG();
 
 			//clear map
-            for(int i=0;i<map_size;i++){
-            	for(int j=0;j<map_size;j++){
-            		terrain_map[j][i] = 0;
-            		item_map[j][i] = 0;
-            	}
-            }
+			for(int i=0;i<map_size;i++){
+				for(int j=0;j<map_size;j++){
+					terrain_map[j][i] = 0;
+					item_map[j][i] = 0;
+				}
+			}
 
-            //water and trees
-            if(addWater)
-            	addNature(5, 1, 0.25, 0.01);               //water
-  			if(addTrees)
-  				addNature(4, 2, 0.20, 0.025);              //tree
+			//water and trees
+			if(addWater)
+				addNature(5, 1, 0.25, 0.01);               //water
+			if(addTrees)
+				addNature(4, 2, 0.20, 0.025);              //tree
+
+			//guarentee key
+			items.add(new Item("key"));
+
+			//make objects
+			for(int e=0;e<Math.round(Math.random()*max_items);e++){
+				String item = (Math.random() < 0.5 ? "hammer" : "key");
+				Item n_item = new Item(item);
+				items.add(n_item);
+				item_map[n_item.y][n_item.x] = 1;
+			}
+
+			//make enemies
+			for(int e=0;e<Math.round(Math.random()*max_enemy);e++){
+				enemies.add(new Enemy());
+			}
+
+			//set the castle and player
+			castle.randomPlace();
+			p1.randomPlace();
+
+			//scene stuff
+			setVisible(true);
+			addKeyListener(this);
+		}
+
+		public void resetMap(){
+			if(game_mode.equals("EASY") || game_mode.equals("NORMAL")){
+				enemies.clear();
+				items.clear();
+			}else{
+				for(Enemy e : enemies){
+					e.randomPlace();
+				}
+			}
 
 
-  			//make objects
+			//clear map
+			for(int i=0;i<map_size;i++){
+				for(int j=0;j<map_size;j++){
+					terrain_map[j][i] = 0;
+					item_map[j][i] = 0;
+				}
+			}
 
-  			//make characters
-  			setVisible(true);
-  			p1.randomPlace();
+			//water and trees
+			double inc = 0;
+			if(game_mode.equals("HARD")){
+				if(p1.castles < 20){
+					inc = p1.castles*0.005;
+				}
+				else if(p1.castles < 50){
+					inc = p1.castles*2*0.005;
+				}else{
+					inc = 0.45;
+				}
+			}
 
-  			addKeyListener(this);
+			if(addWater)
+				addNature(5, 1, 0.25, 0.01);               //water
+			if(addTrees)
+				addNature(4, 2, 0.20 + inc, 0.025);              //tree
+
+			//guarentee key
+			items.add(new Item("key"));
+
+			//make objects
+			for(int e=0;e<Math.round(Math.random()*max_items);e++){
+				String item = (Math.random() < 0.5 ? "hammer" : "key");
+				Item n_item = new Item(item);
+				items.add(n_item);
+				item_map[n_item.y][n_item.x] = 1;
+			}
+
+			//make enemies
+			for(int e=0;e<Math.round(Math.random()*max_enemy);e++){
+				enemies.add(new Enemy());
+			}
+
+			//set the castle and player
+			p1.randomPlace();
+			castle.randomPlace();
+
 		}
 
 		//images
@@ -219,9 +490,9 @@ public class Game{
 				bipi = ImageIO.read(new File("assets/bipi.png")).getScaledInstance(sq_size, sq_size, Image.SCALE_DEFAULT);
 				bridge_h = ImageIO.read(new File("assets/bridge_h.png")).getScaledInstance(sq_size, sq_size, Image.SCALE_DEFAULT);;
 				bridge_v = ImageIO.read(new File("assets/bridge_v.png")).getScaledInstance(sq_size, sq_size, Image.SCALE_DEFAULT);;
-				castle = ImageIO.read(new File("assets/castle.png")).getScaledInstance(sq_size, sq_size, Image.SCALE_DEFAULT);;
-				hammer = ImageIO.read(new File("assets/hammer.png")).getScaledInstance(sq_size, sq_size, Image.SCALE_DEFAULT);;
-				key = ImageIO.read(new File("assets/key.png")).getScaledInstance(sq_size, sq_size, Image.SCALE_DEFAULT);;
+				castle_img = ImageIO.read(new File("assets/castle.png")).getScaledInstance(sq_size, sq_size, Image.SCALE_DEFAULT);;
+				hammer_img = ImageIO.read(new File("assets/hammer.png")).getScaledInstance(sq_size, sq_size, Image.SCALE_DEFAULT);;
+				key_img = ImageIO.read(new File("assets/key.png")).getScaledInstance(sq_size, sq_size, Image.SCALE_DEFAULT);;
 				tree = ImageIO.read(new File("assets/tree.png")).getScaledInstance(sq_size, sq_size, Image.SCALE_DEFAULT);;
 				yeet = ImageIO.read(new File("assets/yeet.png")).getScaledInstance(sq_size, sq_size, Image.SCALE_DEFAULT);;
 			}catch(IOException e){}
@@ -241,35 +512,62 @@ public class Game{
 			*/
 
 			//grass
-            g.setColor(new Color(106, 190, 48));
-            g.fillRect(sq_size, sq_size, grid_size, grid_size);
+			g.setColor(new Color(106, 190, 48));
+			g.fillRect(sq_size, sq_size, grid_size, grid_size);
 
- 			//terrain and items
-            for(int y=0;y<map_size;y++){
-            	for(int x=0;x<map_size;x++){
-       				int tm = terrain_map[y][x];
-       				int im = item_map[y][x];
+			//terrain and items
+			for(int y=0;y<map_size;y++){
+				for(int x=0;x<map_size;x++){
+					int tm = terrain_map[y][x];
+					int im = item_map[y][x];
 
-       				int cellX = sq_size + (x * sq_size);
+					int cellX = sq_size + (x * sq_size);
 					int cellY = sq_size + (y * sq_size);
 					
 					//water
-            		if(tm == 1){
-            			g.setColor(new Color(142, 207, 221));
+					if(tm == 1){
+						g.setColor(new Color(142, 207, 221));
 						g.fillRect(cellX, cellY, sq_size, sq_size);
-            		}
-            		//tree
-            		else if(tm == 2){
-            			g.drawImage(tree, cellX, cellY, null);
-            		}		
-            	}
-            }
+					}
+					//tree
+					else if(tm == 2){
+						g.drawImage(tree, cellX, cellY, null);
+					}		
+				}
+			}
 
-            //player
-            int px = sq_size + (p1.x * sq_size);
-            int py = sq_size + (p1.y * sq_size);
-            g.drawImage(bipi, px, py, null);
+			//draw castle
+			g.drawImage(castle_img, sq_size + (castle.x * sq_size), sq_size + (castle.y * sq_size), null);
+			
+			//draw items
+			for(Item it : items){
+				if(it.show){
+					int ix = sq_size + (it.x * sq_size);
+					int iy = sq_size + (it.y * sq_size);
+					if(it.name.equals("hammer")){
+						g.drawImage(hammer_img, ix, iy, null);
+					}else if(it.name.equals("key")){
+						g.drawImage(key_img, ix, iy, null);
+					}
+				}
+			}
 
+			//player
+			int px = sq_size + (p1.x * sq_size);
+			int py = sq_size + (p1.y * sq_size);
+			if(p1.show){
+				g.drawImage(bipi, px, py, null);
+			}
+			
+
+			//draw enemies
+			for(Enemy en : enemies){
+				if(en.show){
+					int ex = sq_size + (en.x * sq_size);
+					int ey = sq_size + (en.y * sq_size);
+					g.drawImage(yeet, ex, ey, null);
+				}
+			}
 
 			//grid
 			if(showGrid){
@@ -301,9 +599,9 @@ public class Game{
 		public void addNature(int multi, int obj, double prob, double dec){
 		  int range = (int)(Math.floor(Math.random() * multi) + 1);
 		  for(int a = 0; a < map_size; a+=(range)){
-		    for(int b = 0; b < map_size; b+=(range)){
-		      generate(obj, prob, dec, b, a);
-		    }
+			for(int b = 0; b < map_size; b+=(range)){
+			  generate(obj, prob, dec, b, a);
+			}
 		  }
 		}
 
@@ -311,22 +609,22 @@ public class Game{
 		public void generate(int obj, double prob, double dec, int x, int y){
 		  double r = Math.random();
 		  if(r < prob){
-		    make(obj, x, y);
+			make(obj, x, y);
 
-		    //go in each direction
-		    generate(obj, prob - dec, dec, x, y+1);
-		    generate(obj, prob - dec, dec, x, y-1);
-		    generate(obj, prob - dec, dec, x+1, y);
-		    generate(obj, prob - dec, dec, x-1, y);
+			//go in each direction
+			generate(obj, prob - dec, dec, x, y+1);
+			generate(obj, prob - dec, dec, x, y-1);
+			generate(obj, prob - dec, dec, x+1, y);
+			generate(obj, prob - dec, dec, x-1, y);
 		  }else{
-		    return;
+			return;
 		  }
 		}
 
 		//create a terrain tile at the specific pt on the map
 		public void make(int obj, int x, int y){
 		  if((x >= 0 && x < map_size) && (y >= 0 && y < map_size) && terrain_map[y][x] == 0)
-		    terrain_map[y][x] = obj;
+			terrain_map[y][x] = obj;
 		}
 
 
@@ -334,7 +632,8 @@ public class Game{
 		@Override
 		public void keyPressed(KeyEvent e){
 			int code = e.getKeyCode();
-			if(move){
+			if(move && p1.show){
+				//player movement
 				if(code == 40){
 					p1.walk("south");
 				}else if(code == 38){
@@ -344,8 +643,31 @@ public class Game{
 				}else if(code == 39){
 					p1.walk("east");
 				}
+
+				//enemy movement
+				for(Enemy en : enemies){
+					en.walk();
+				}
+
+				//check for overlaps
+				for(Item item : items){
+					if(item.x == p1.x && item.y == p1.y && item.show){
+						p1.pickup(item);
+					}
+				}
+
+				for(Enemy enemy : enemies){
+					if(enemy.x == p1.x && enemy.y == p1.y && enemy.show){
+						enemy.duel();
+						System.out.println("fight me!");
+					}
+				}
+
+
+				//redraw everything
 				this.repaint();
 				move = false;
+				game_info.updateSettings();
 			}
 
 		}
@@ -356,6 +678,9 @@ public class Game{
 		public void keyReleased(KeyEvent e){
 			move = true;
 		}
+
+		
+
 	}
 
 	public static class Settings extends JPanel{
@@ -370,16 +695,16 @@ public class Game{
 
 			List<JLabel> set_labels = new ArrayList<JLabel>();
 
-			hammerLbl = new JLabel("Hammers: 0");
+			hammerLbl = new JLabel("Hammers: " + p1.hammer);
 			set_labels.add(hammerLbl);
 
-			keysLbl = new JLabel("Keys: 0");
+			keysLbl = new JLabel("Keys: " + p1.key);
 			set_labels.add(keysLbl);
 
-			castlesLbl = new JLabel("Castles: 0");
+			castlesLbl = new JLabel("Castles: " + p1.castles);
 			set_labels.add(castlesLbl);
 
-			enemiesLbl = new JLabel("Enemies: 0");
+			enemiesLbl = new JLabel("Enemies: " + p1.enemies);
 			set_labels.add(enemiesLbl);
 
 			for(JLabel lbl : set_labels){
@@ -398,6 +723,13 @@ public class Game{
 			treesCheck.setSelected(addTrees);
 			*/
 		}
+		public void updateSettings(){
+			hammerLbl.setText("Hammers: " + p1.hammer);
+			keysLbl.setText("Keys: " + p1.key);
+			castlesLbl.setText("Castles: " + p1.castles);
+			enemiesLbl.setText("Enemies: " + p1.enemies);
+		}
+
 	}
 
 	public static class User_Input extends JPanel implements ActionListener{
@@ -530,7 +862,7 @@ public class Game{
 		grid.setSize(grid_size+(sq_size*2), grid_size+(sq_size*2));
 		allGUI.add(grid);
 
-		System.out.println("Player: " + p1.x + ", " + p1.y);
+		//System.out.println("Player: " + p1.x + ", " + p1.y);
 
 		int in_size = (int)(grid_size*(2.0/5.0));
 		game_info.setLocation(grid_size+(sq_size*2)+(sq_size), sq_size);
